8375857051827dfb2a4df473a28ec2cd46645307
Victor Lima <lima.p.victor@outlook.com>
2020-07-06 23:34:09 -0300

    [CUDA]Teste -> Incluida medição usada no EP1

--- src/mandelbrot_cu.cu
+++ src/mandelbrot_cu.cu
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <time.h>
+#include <sys/time.h>
 
 #define GRADIENT_SIZE 16
 
@@ -48,6 +50,17 @@ unsigned char *d_image_buffer;
 cudaEvent_t allocation_start, allocation_end;
 cudaEvent_t computing_start, computing_end;
 float allocation_time, computing_time;
+
+struct timer_info {
+    clock_t c_start;
+    clock_t c_end;
+    struct timespec t_start;
+    struct timespec t_end;
+    struct timeval v_start;
+    struct timeval v_end;
+};
+
+struct timer_info computing_timer;
 
 void init(int argc, char *argv[])
 {
@@ -137,12 +150,28 @@ void compute_mandelbrot()
     cudaMemcpy(image_buffer, d_image_buffer, ARRAY_SIZE, cudaMemcpyDeviceToHost);
 }
 
+void start_timer(struct timer_info *timer_pointer) 
+{
+    timer_pointer->c_start = clock();
+    clock_gettime(CLOCK_MONOTONIC, &timer_pointer->t_start);
+    gettimeofday(&timer_pointer->v_start, NULL);
+}
+
+void finish_timer(struct timer_info *timer_pointer)
+{
+    timer_pointer->c_end = clock();
+    clock_gettime(CLOCK_MONOTONIC, &timer_pointer->t_end);
+    gettimeofday(&timer_pointer->v_end, NULL);
+}
+
 int main(int argc, char *argv[])
 {
     init(argc, argv);
 
     cudaEventRecord(computing_start, 0);
+    start_timer(&computing_timer);
     compute_mandelbrot();
+    finish_timer(&computing_timer);
     cudaEventRecord(computing_end, 0);
 
     write_to_file();
@@ -154,5 +183,8 @@ int main(int argc, char *argv[])
     cudaEventElapsedTime(&computing_time, computing_start, computing_end);
 
     printf("Allocation Time = %.6f\tComputing Time = %.6f\n", allocation_time, computing_time);
+    printf("Computing timer test = %f\n",
+            (double) (program_timer.t_end.tv_sec - program_timer.t_start.tv_sec) +
+            (double) (program_timer.t_end.tv_nsec - program_timer.t_start.tv_nsec) / 1000000000.0);
     return 0;
 }
